4a8d58e4663a1e646bb3b7f7faa6eecbab5bd5d8
|VOID| <rowlandkhd@gmail.com>
2026-08-04 15:02:10 -0500

    Test ParcelsApp web protocol encoding

---
+++ app/src/test/java/com/voidnullvalue/parcelpal/source/ParcelsAppProtocolTest.java
@@ -0,0 +1,20 @@
+package com.voidnullvalue.parcelpal.source;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public final class ParcelsAppProtocolTest {
+    @Test public void reproducesPublicWebTrackingIdTransform() {
+        assertEquals(
+                "%07%02%7C%7C%7D%7D%7D%00%7C%04%00%7D%01%05%06%03%04%05%06%07%7C%7D",
+                ParcelsAppProtocol.encodedTrackingId("9400111206213785678901")
+        );
+    }
+
+    @Test public void mapsDetectedCarrierToParcelsSlug() {
+        assertEquals("usps", ParcelsAppProtocol.carrierSlug("USPS"));
+        assertEquals("fedex", ParcelsAppProtocol.carrierSlug("FedEx"));
+        assertEquals("", ParcelsAppProtocol.carrierSlug("Auto-detect"));
+    }
+}
